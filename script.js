0e4b5f7ea47266f79afe217fd29ec12b36a8eff0
mich0422 <eguim870@newschool.edu>
2015-05-12 09:24:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+
+//THIS IS THE HOURGLASS for final(color change, timing change)
+ 
+
+
+
 var a;
 var b;
+var c;
 var direction;
+var d;
+var randomColorChange = "";
 
 
 
@@ -9,31 +18,64 @@ function setup() {
   colorMode(RGB, width);
   a = 0;
   b = width;
+  c = 0;
+  d = height;
   direction = true;
   frameRate(50);
 }
 
 function draw() {
-  a+=28;
+  // d=mouseY;
+
+
+
+
+//this is the SECONDS hourglass
+
+  a+=29;
   if(a > width) {
     a = 0;
     direction = !direction;
   }
   if(direction == true){
-    stroke(a-90);
+    stroke(a, width-a);
   } else {
-    stroke(width+a);
+    stroke(width+a, width*a+200);
   }
-  line(a, a/2, a*3, height*3-30);
 
-  b-=.50;
+  line(0, 20+a*9, 450-a, height*0.00001);
+
+
+
+//this is the MINUTES hourglass
+
+  b-=.52;
   if(b < 0) {
     b = width;
   }
   if(direction == true) {
-    stroke(0, width-6*b, 4*width-b);
+    stroke(height-b, 0, 20);
   } else {
-    stroke(width-b, width*b, 2*width-b);
+    stroke(40*width+b, b, b);
   }
-  line(2*height-b, height/20+5, height+b, height);
+
+  line(1.7*height-b, height/500+5, height+0.75*b, height);
+
+
+
+//this is the HOUR hourglass
+
+  c+=.00933;
+  if(c < 0) {
+  c = width;
+  }
+  if(direction == true) {
+    stroke(d*400, d/200, 0);
+  } else {
+    stroke(400*width+c, c, c);
+  }
+
+  line(1.7*d-c, d/100+5, c/2, d);
+  
+
 }
